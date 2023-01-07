167a7edd0e2cab19d8115e1e309ee3b9c6dd1f8d
RohitNGiri <procoderpc@gmail.com>
2023-01-07 13:56:50 +0530

    add send message functionality

--- src/components/Contact/Contact.component.jsx
+++ src/components/Contact/Contact.component.jsx
@@ -4,6 +4,8 @@ import './Contact.styles.css';
 import { FaLocationArrow } from 'react-icons/fa';
 import { BsFillTelephoneFill } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai';
+import gif1 from '../../assets/gif1.gif';
+
 
 const Contact = () => {
     const [error, setError] = useState(false);
@@ -11,6 +13,7 @@ const Contact = () => {
     const handleChange = (e) => {
         e.preventDefault();
         setError(true);
+
 
     }
 
@@ -58,31 +61,32 @@ const Contact = () => {
 
                                             </div>
                                             <div>
+                                                {error && <div className="succmessage">Your Message as been sent Successfully! <img src={gif1} /></div>}
                                                 <form action="" onSubmit={handleChange} className={`${error ? 'error' : 'contactForm'}`} role="form">
-                                                    <div id="sendmessage">Your message has been sent. Thank you!</div>
+
                                                     <div id="errormessage"></div>
                                                     <div class="row">
                                                         <div class="col-md-12 mb-3">
                                                             <div class="form-group">
-                                                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                                                <input type="text" name="name" required class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                                                 <div class="validation"></div>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-12 mb-3">
                                                             <div class="form-group">
-                                                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                                                <input type="email" required class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                                                                 <div class="validation"></div>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-12 mb-3">
                                                             <div class="form-group">
-                                                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                                                <input type="text" required class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
                                                                 <div class="validation"></div>
                                                             </div>
                                                         </div>
                                                         <div class="col-md-12 mb-3">
                                                             <div class="form-group">
-                                                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                                                <textarea class="form-control" required name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
                                                                 <div class="validation"></div>
                                                             </div>
                                                         </div>
